20e3934134f514a4fb16385f47418874464d2926
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-09-26 10:14:06 -0400

    verified accuracy for axis and discriminant

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,17 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         //todo declare variable a,b,c, discriminant ,axisOFSymmetry. parabolaFacing x,y
-        double a,b,c,discriminant,axisOFSymmentry,x,y;
+        double a,b,c,discriminant,axisOFSymmetry,x,y;
         String parabolaFacing;
         //todo initialize manually
         a=5;
         b=3;
         c=1;
         //todo calculations
-        axisOFSymmentry = (-b/2*a);
+        axisOFSymmetry = -b/(2*a);
         discriminant = (b*b)-4*(a*c);
         //todo print results
-
+        System.out.println("Axis of Symmetry: "+ axisOFSymmetry);
+        System.out.println("Discriminant: "+ discriminant);
         //todo not in code check results
 
         //todo initialize scanner and redo the initialize area
