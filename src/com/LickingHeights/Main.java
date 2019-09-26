52ef8d41d77d09b37521357cd4352487ee832753
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-09-26 09:55:45 -0400

    added todo steps

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,23 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //todo declare variable a,b,c, discriminant ,axisOFSymmetry. parabolaFacing x,y
+        
+        //todo initialize manually
+
+        //todo calculations
+
+        //todo print results
+
+        //todo not in code check results
+
+        //todo initialize scanner and redo the initialize area
+
+        //todo double check results
+
+
+    }
+}
