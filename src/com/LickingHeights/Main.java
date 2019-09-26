987316ff5e1e5881a15dc6a0b811e66695ca1a41
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-09-26 10:02:02 -0400

    initialized the variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,12 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         //todo declare variable a,b,c, discriminant ,axisOFSymmetry. parabolaFacing x,y
-        double a,b,d,discriminant,axisOFSymmentry,x,y;
-                String parabolaFacing;
+        double a,b,c,discriminant,axisOFSymmentry,x,y;
+        String parabolaFacing;
         //todo initialize manually
-
+        a=5;
+        b=3;
+        c=1;
         //todo calculations
-
+        
         //todo print results
 
         //todo not in code check results
