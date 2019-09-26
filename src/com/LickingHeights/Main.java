789a6115bed55a07adccf289bdb671bc1e91d39e
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-09-26 10:00:13 -0400

    declare variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         //todo declare variable a,b,c, discriminant ,axisOFSymmetry. parabolaFacing x,y
-        
+        double a,b,d,discriminant,axisOFSymmentry,x,y;
+                String parabolaFacing;
         //todo initialize manually
 
         //todo calculations
